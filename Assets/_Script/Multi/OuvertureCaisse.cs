86e60da4de986e1d7d091b6e531dc7437350ac51
Stephanie R <stephanie.ruiz@epita.fr>
2016-05-01 08:08:37 +0200

    sauvegarde checkpoint avant essai multi caisse

---
+++ Assets/_Script/Multi/OuvertureCaisse.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OuvertureCaisse : MonoBehaviour
+{
+    private bool finis;
+    public Animator Anim;
+    public AudioSource Sond;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Nez")
+        {
+            if (finis)
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    Anim.enabled = true;
+                    Sond.Play();
+                    finis = false;
+                }
+            }
+        }
+    }
+}
+
