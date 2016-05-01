ac6ae56de82b6b1fe277c4aa5d30097e873a1118
Stephanie R <stephanie.ruiz@epita.fr>
2016-05-01 08:45:55 +0200

    Caisse multi reuissi :D

--- Assets/_Script/Multi/OuvertureCaisse.cs
+++ Assets/_Script/Multi/OuvertureCaisse.cs
@@ -10,7 +10,7 @@ public class OuvertureCaisse : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        finis = true;
     }
 
     // Update is called once per frame
@@ -20,12 +20,15 @@ public class OuvertureCaisse : MonoBehaviour
     }
     void OnTriggerStay(Collider other)
     {
+        
         if (other.gameObject.tag == "Nez")
         {
+            //Debug.Log("Nez en vu de caisse");
             if (finis)
             {
                 if (Input.GetKeyDown(KeyCode.E))
                 {
+                    //Debug.Log("Essai d ouvrir caisse");
                     Anim.enabled = true;
                     Sond.Play();
                     finis = false;
