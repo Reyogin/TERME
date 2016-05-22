10306d1fbdd70ab65397a41a05236d75904568f1
Bertrand Ha <bertrand.ha@epita.fr>
2016-05-22 16:49:56 +0200

    Nav mesh Fixed + rework GUI ready to push

---
+++ Assets/AIScript_master.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIScript_master : MonoBehaviour
+{
+    private Transform target;
+    Animator animator;
+    NavMeshAgent nav;
+    float timer;
+    private float timebetweenatks = 2.0f;
+    private float range = 2f;
+    private float distance;
+    private float damage = 10f;
+    // Use this for initialization
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        MoveTowards();
+        Attack();
+    }
+
+    void MoveTowards()
+    {
+        Vector3 targetDir = target.position - transform.position;
+        Vector3 forward = transform.forward;
+
+        float distance = Vector3.Distance(transform.position, target.position);
+        float angle = Vector3.Angle(targetDir, forward);
+
+        if (angle < 45f)
+        {
+            if (distance <= 25f)
+            {
+                nav.SetDestination(target.position);
+                animator.SetBool("Sees Enemy", true);
+            }
+            else
+                animator.SetBool("Sees Enemy", false);
+        }
+
+        if (target.GetComponent<GUI_HealthPlayer>().currentHealth <= 0)
+        {
+            nav.Stop();
+            animator.SetBool("Sees Enemy", false);
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            nav.SetDestination(target.position);
+            animator.SetBool("Sees Enemy", true);
+        }
+        else
+            animator.SetBool("Sees Enemy", false);
+    }
+
+    void Attack()
+    {
+        Vector3 targetDir = target.position - transform.position;
+        Vector3 forward = transform.forward;
+
+        float distance = Vector3.Distance(transform.position, target.position);
+        float angle = Vector3.Angle(targetDir, forward);
+
+        if (angle < 45f)
+        {
+            if (distance <= range && timer >= timebetweenatks)
+            {
+                timer = 0f;
+                animator.SetTrigger("Atk");
+                target.transform.SendMessage("TakingPunishment", damage);
+            }
+            animator.SetTrigger("Rest");
+
+            if (target.GetComponent<GUI_HealthPlayer>().currentHealth <= 0)
+            {
+                animator.SetBool("Sees Enemy", false);
+                animator.SetTrigger("Rest");
+            }
+        }
+    }
+}
+
--- Assets/_Script/Solo/Gameplay/Player/GUI_HealthPlayer.cs
+++ Assets/_Script/Solo/Gameplay/Player/GUI_HealthPlayer.cs
@@ -21,7 +21,7 @@ public class GUI_HealthPlayer : CQCCombat
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Combat Status : " + combatStatus +"\n leavecombat = " + leavecombat);
+        Debug.Log("Combat Status : " + combatStatus + "\n leavecombat = " + leavecombat);
         regenGP(combatStatus);
         if (Input.GetKeyDown(KeyCode.C))
             TakingPunishment(10);
@@ -42,7 +42,7 @@ public class GUI_HealthPlayer : CQCCombat
     /// </summary>
     /// <param name="position"> = position on the screen</param>
     /// <param name="color"> Color of the rectangle drawn</param>
-    public static void GUIDrawRect(Rect position, Color color) 
+    public static void GUIDrawRect(Rect position, Color color)
     {
         if (_staticRectTexture == null)
             _staticRectTexture = new Texture2D(1, 1);
@@ -61,7 +61,7 @@ public class GUI_HealthPlayer : CQCCombat
     public static void GUIDrawHP(float percent)
     {
         if (percent > 0.5f)
-            GUIDrawRect(new Rect(162, Screen.height - 70, 228 * percent, 8), Color.green);
+            GUIDrawRect(new Rect(162, Screen.height - 70, 228 * percent, 8), new Color(61/255f,161/255f,74/255f,1));
         else if (percent > 0.2f)
             GUIDrawRect(new Rect(162, Screen.height - 70, 228 * percent, 8), Color.yellow);
         else
