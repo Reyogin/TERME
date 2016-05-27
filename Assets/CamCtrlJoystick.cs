702b81a4f5fec47519117745339b5e2071c805ee
Bertrand Ha <bertrand.ha@epita.fr>
2016-05-27 20:22:06 +0200

    Cam Rotate on horizontal, not on vertical

---
+++ Assets/CamCtrlJoystick.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CamCtrlJoystick : MonoBehaviour
+{
+    private Transform playerTransform;
+    private GameObject cam;
+    private float rotationSpeed = 150.0f;
+
+    private float minx = -70.0f;
+    private float maxx = 55.0f;
+    private Vector3 v3rotate = Vector3.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerTransform = GetComponent<Transform>();
+        this.cam = gameObject.GetComponentInChildren<Camera>().gameObject;
+        cam.transform.localEulerAngles = v3rotate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.GetAxis("RightJoystickY"));
+        GetInput();
+    }
+
+    void GetInput()
+    {
+        transform.Rotate(0.0f, Input.GetAxis("RightJoystickX") * rotationSpeed * Time.deltaTime, 0.0f);
+        v3rotate.x -= Input.GetAxis("RightJoystickY") * rotationSpeed * Time.deltaTime;
+        v3rotate.x = Mathf.Clamp(v3rotate.x, minx, maxx);
+        this.cam.transform.localEulerAngles = v3rotate;
+    }
+}
