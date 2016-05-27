d9da93995a35ade04afc2a88aa055228b23787d5
Reyogin <ha_d@epita.fr>
2016-05-27 10:44:06 +0200

    Commented idea for distance fights

--- Assets/_Script/Multi/WeaponHudUpDate.cs
+++ Assets/_Script/Multi/WeaponHudUpDate.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class WeaponHudUpDate : MonoBehaviour
 {
-
+    private CQCCombat CQC_script;
     private Transform playerTransform;
     private List<Transform> listeItems;
     private List<Transform> AncieneListeItems;
@@ -15,6 +15,7 @@ public class WeaponHudUpDate : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        CQC_script = GetComponent<CQCCombat>();//a deactivate si index == 1 => on va faire ca pour activer les combats a distance
         playerTransform = GetComponent<Transform>();
         AncieneListeItems = new List<Transform>();
     }
