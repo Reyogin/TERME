81f32638bd3520c0b383c6289d6fdbbf62d210ed
Reyogin <ha_d@epita.fr>
2016-05-31 16:32:55 +0200

    Debut du travail

---
+++ Assets/Boomstick.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boomstick : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletspawn;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Fire1"))
+            Shoot();
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, bulletspawn.position, bulletspawn.rotation);
+    }
+}
---
+++ Assets/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+    private float speed;
+	// Use this for initialization
+	void Start ()
+    {
+        speed = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position += transform.forward * Time.deltaTime * speed;
+	}
+}
