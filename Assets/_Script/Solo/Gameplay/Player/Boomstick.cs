3a3240b2958761a8f0e3eff1207806a79d91d1f7
Bertrand Ha <bertrand.ha@epita.fr>
2016-06-16 14:40:53 +0200

    Anim shooting good, same with timing. TODO : REWORK ANIMATOR, Animations + think about Combat system

--- Assets/_Script/Solo/Gameplay/Player/Boomstick.cs
+++ Assets/_Script/Solo/Gameplay/Player/Boomstick.cs
@@ -14,15 +14,13 @@ public class Boomstick : MonoBehaviour {
 
     //Audio du coup de feu
     AudioClip bang;
-
-    //Animation de tir
-    Animation anim;
-
+    Animator anim;
+   
     // Use this for initialization
     void Start()
     {
         bang = Resources.Load<AudioClip>("Sound/MusketFire");
-        anim = GetComponent<Animation>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,29 +28,32 @@ public class Boomstick : MonoBehaviour {
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            anim.Play("shooting_gun");
-
-            //The Bullet instantiation happens here.
-            GameObject Temporary_Bullet_Handler;
-            Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
-
-            //Sometimes bullets may appear rotated incorrectly due to the way its pivot was set from the original modeling package.
-            //This is EASILY corrected here, you might have to rotate it from a different axis and or angle based on your particular mesh.
-            //Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
-
-            //Retrieve the Rigidbody component from the instantiated Bullet and control it.
-            Rigidbody Temporary_RigidBody;
-            Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
-
-            //Tell the bullet to be "pushed" forward by an amount set by Bullet_Forward_Force.
-            Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force);
-
-            //BANG!
-            AudioSource.PlayClipAtPoint(bang, Bullet_Emitter.transform.position);
-
-            //Basic Clean Up, set the Bullets to self destruct after 10 Seconds, I am being VERY generous here, normally 3 seconds is plenty.
-            Destroy(Temporary_Bullet_Handler, 10.0f);
+            Invoke("instantiateBullet", 0.8f);
         }
+    }
+
+    void instantiateBullet()
+    {
+        //The Bullet instantiation happens here.
+        GameObject Temporary_Bullet_Handler;
+        Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+
+        //Sometimes bullets may appear rotated incorrectly due to the way its pivot was set from the original modeling package.
+        //This is EASILY corrected here, you might have to rotate it from a different axis and or angle based on your particular mesh.
+        //Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+
+        //Retrieve the Rigidbody component from the instantiated Bullet and control it.
+        Rigidbody Temporary_RigidBody;
+        Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+
+        //Tell the bullet to be "pushed" forward by an amount set by Bullet_Forward_Force.
+        Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force);
+
+        //BANG!
+        AudioSource.PlayClipAtPoint(bang, Bullet_Emitter.transform.position);
+
+        //Basic Clean Up, set the Bullets to self destruct after 10 Seconds, I am being VERY generous here, normally 3 seconds is plenty.
+        Destroy(Temporary_Bullet_Handler, 10.0f);
     }
 }
 
