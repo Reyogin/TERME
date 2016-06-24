990969aac95f40e570ae764b887e780754b4685a
Bertrand Ha <bertrand.ha@epita.fr>
2016-06-25 01:00:56 +0200

    First commit on adapting scripts to multi

---
+++ Assets/_Script/Multi/Gameplay/Combat_multi.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Combat_multi : NetworkBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
