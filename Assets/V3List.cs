0118e01950059634a562dcd69373ae95d011f95e
Bertrand Ha <bertrand.ha@epita.fr>
2016-06-26 18:01:21 +0200

    Started making list of V3 for patrolling

---
+++ Assets/V3List.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class V3List : MonoBehaviour
+{
+    public List<Vector3> liste;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
