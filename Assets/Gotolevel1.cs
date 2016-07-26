7da87c3f205410dc45728db4c268ffabde5ea8bf
Stephanie R <stephanie.ruiz@epita.fr>
2016-07-26 10:13:31 +0200

    level 1 partielle;ent fix

--- Assets/Gotolevel1.cs
+++ Assets/Gotolevel1.cs
@@ -9,6 +9,6 @@ public class Gotolevel1 : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
-            SceneManager.LoadScene("Level 2");
+            SceneManager.LoadScene("Level 1");
     }
 }
---
+++ Assets/_Script/Solo/Maze/SpikesActivation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikesActivation : MonoBehaviour
+{
+    public GameObject spikes;
+
+    void Start()
+    {
+        //spikes = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            
+            spikes.SetActive(true);
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            spikes.SetActive(false);
+        }
+    }
+}
